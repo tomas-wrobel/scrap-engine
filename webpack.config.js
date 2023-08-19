0b64c551b39d553eb4b83f3d59c540c04c658f03
Tomáš Wróbel <tomas.wrobel@email.cz>
2023-08-20 00:23:56 +0200

    Removing devtool

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path')
 
 module.exports = {
   entry: './src/engine.js',
-  mode: 'development',
   output: {
     filename: "engine.js",
     path: path.resolve(__dirname, 'dist'),
