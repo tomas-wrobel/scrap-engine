78e2d44e5ebafc41a2b61f80d722f8f3ca666939
Tomáš Wróbel <tomas.wrobel@email.cz>
2023-08-19 22:59:50 +0200

    Improving ESLint

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,6 @@ const path = require('path')
 module.exports = {
   entry: './src/lib.js',
   mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/, // both .js and .jsx
-        loader: 'eslint-loader',
-        include: path.resolve(process.cwd(), 'src'),
-        enforce: 'pre',
-        options: {
-          fix: true
-        }
-      }
-    ]
-  },
   devtool: 'inline-source-map',
   output: {
     filename: `engine.js`,
